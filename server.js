008f080ae5eade056af27c2141cc9b88bb346ce2
JG <gravestocker@gmail.com>
2021-12-04 12:47:24 -0800

    backend ready to deploy

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import { readdirSync } from "fs";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
+const morgan = require("morgan");
+require("dotenv").config();
+
+const csrfProtection = csrf({ cookie: true });
+
+// create express server
+const app = express();
+
+// db connection using native Node MongoDB connection
+// const { MongoClient } = require('mongodb');
+// const uri = process.env.DATABASE;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   console.log('DB SUCCESS!')
+//   // perform actions on the collection object
+//   client.close();
+//   console.log('DB Connection Terminated');
+// });
+//       *Alternate using mongoose*
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection established"))
+  .catch((err) => console.error("DB CONNECTION ERR => ", err));
+
+// apply middleware
+app.use(cors());
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// route
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+// csrf
+app.use(csrfProtection);
+
+app.get("/api/csrf-token", (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
